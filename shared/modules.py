a75ff096efb133a05e5dcaf9849a257fc98aafba
Gustavo Castillo <gustavoosc_@hotmail.com>
2025-06-16 15:20:04 -0300

    Refatoração do .venv + ajustes no requirements.txt

--- shared/modules.py
+++ shared/modules.py
@@ -1,9 +1,8 @@
-from .utils import generate_unique_id, format_date, save_json_to_file, log_body_request, extract_json_data
+from .utils import generate_unique_id, format_date, save_json_to_file, extract_json_data
 
 __all__ = [
     "generate_unique_id",
     "format_date",
     "save_json_to_file",
-    "log_body_request",
     "extract_json_data"
 ]
